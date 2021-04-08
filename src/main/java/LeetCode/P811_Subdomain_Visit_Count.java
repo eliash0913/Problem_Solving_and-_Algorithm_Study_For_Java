dd8ad4cc7b18d8a68981b09c8f9b4858390530a3
eliash0913 <eliash0913@gmail.com>
2021-04-08 14:06:55 -0700

    "~20210408 Study"

--- src/main/java/LeetCode/P811_Subdomain_Visit_Count.java
+++ src/main/java/LeetCode/P811_Subdomain_Visit_Count.java
@@ -1,11 +1,35 @@
 package LeetCode;
 
-import java.util.List;
+import java.util.*;
 
 public class P811_Subdomain_Visit_Count {
     public List<String> subdomainVisits(String[] cpdomains) {
-        List<String> result;
+        HashMap<String, Integer> hmap = new HashMap<>();
+        for(String cpdomain : cpdomains){
+            String[] cpdomainArray = cpdomain.split(" ");
+            Integer count = Integer.parseInt(cpdomainArray[0]);
+            String domain = cpdomainArray[1];
+            String[] subDomain = domain.split("\\.");
+            hmap.computeIfPresent(domain, (key,val)->val+count);
+            hmap.putIfAbsent(domain, count);
+            if(subDomain.length==3){
+                subDomain = domain.split("\\.",2);
+                hmap.computeIfPresent(subDomain[1], (key, val) -> val+count);
+                hmap.putIfAbsent(subDomain[1], count);
+                subDomain = subDomain[1].split("\\.",2);
+            }
+            if(subDomain.length==2){
+                hmap.computeIfPresent(subDomain[1], (key, val) -> val+count);
+                hmap.putIfAbsent(subDomain[1], count);
+            }
 
+        }
+
+        List<String> result = new ArrayList<>();
+        for(Map.Entry entry : hmap.entrySet()){
+            String e = entry.getValue().toString()+" "+entry.getKey();
+            result.add(e);
+        }
 
         return result;
     }
